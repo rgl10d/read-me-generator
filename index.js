a015ef3942d10b80f5c93946fe8dd46b3b59f7dc
Garrett Lee <rgl10d@gmail.com>
2020-11-02 21:49:25 -0500

    added more prompted questions and wrote out the resulting readme

--- index.js
+++ index.js
@@ -1,62 +1,61 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-inquirer.prompt([
+const questions = [
+
+     {
+       type: "input",
+       name: "title",
+       message: "What is the project's title?",
+     },
+     {
+       type: "input",
+       name: "description",
+       message: "Enter a description for the project.",
+     },
+     {
+       type: "input",
+       name: "installation",
+       message: "How is the program installed?",
+     },
+     {
+       type: "input",
+       name: "usage",
+       message: "Explain how to operate/use the project.",
+     },
+     {
+       type: "input",
+       name: "contribution",
+       message: "Enter information on any project contributors.",
+     },
+     {
+       type: "input",
+       name: "github",
+       message: "Enter your GitHub profile link.",
+     },
+         {
+       type: "input",
+       name: "email",
+       message: "Enter your email address.",
+     },
     {
-      type: "input",
-      name: "title",
-      message: "What is the project's title?",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Enter a description for the project.",
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "How is the program installed?",
-    },
-    {
-      type: "list",
-      name: "usage",
-      message: "Select a usage license.",
-      choices: ["MIT License"]
-    },
-    {
-      type: "input",
-      name: "contribution",
-      message: "Enter information on any project contributors.",
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Enter your GitHub profile link.",
-    },
-        {
-      type: "input",
-      name: "email",
-      message: "Enter your email address.",
-    },
-  ])
-  .then((answers) => {
-    const readmeText = `This is the readme text with the ${answers.title} title.`
-    fs.writeFile("NEWREADME.md", readmeText, (err) =>
-        err ? console.error(err) : console.log("Commit logged!")
-    );
-  });
+       type: "list",
+       name: "license",
+       message: "Select a usage license.",
+       choices: ["MIT License"]
+     },
+];
+
 // function to write README file
 // function writeToFile(fileName, data) {
 // }
 
-
-// function to initialize program
-// function init() {
-
-// }
-
-// function call to initialize program
-// init();
-
-
+inquirer.prompt(questions)
+  .then((answers) => {
+    // const readmeText = `This is the readme text with the ${answers.title} title.`
+    fs.writeFile("NEWREADME.md", generateMarkdown(answers), (err) =>
+        err ? console.error(err) : console.log("ReadMe created!")
+    );
+  });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,48 @@
+
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+## Description 
+
+${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+## Installation
+
+${data.installation}
+
+## Usage 
+
+${data.usage}
+
+
+## Credits
+
+${data.contribution}
+
+## License
+
+${data.license}
+
+---
+
+## Questions?
+You can visit me at my GitHub page: ${data.github}
+Or email me at: ${data.email}
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+## Tests
 
 `;
 }
